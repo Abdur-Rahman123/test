e0893c1fb1dd468224dd0d2e903346cc45b5a036
Rahman <rahman.abdur4420@gmail.com>
2022-09-17 19:17:44 +0600

    add main routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashbordComponent } from './dashbord/dashbord.component';
 
+
 const routes: Routes = [
+  { path: '',   redirectTo: '/dashbord', pathMatch: 'full' }, 
   {path:'dashbord',component:DashbordComponent}
 ];
 
